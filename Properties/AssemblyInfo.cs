a8b0c7691f5ce4e340ed9d5563936c4264b23408
Ivy Guo <gj1002@gmail.com>
2019-12-06 10:03:53 +1300

    connect sql server

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SQLConnection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQLConnection")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0da1fa4d-2ec6-4d21-81bc-9d235c4b5b38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using NUnit.Framework;
+using System.Data;
+
+namespace SQLConnection
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestConnection()
+        {
+            string connectionString=@"Server=IVYGUO1002\SQLEXPRESS;Database=Test;Trusted_Connection=SSPI";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Do work here; connection closed on following line.  
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                    connection.ConnectionString);
+            }
+
+        }
+        [Test]
+        public void TestNonQuery()
+        {
+            string connectionString =@"Server = IVYGUO1002\SQLEXPRESS; Database = Test; Trusted_Connection = SSPI";
+            string queryString = "Insert Into Persons Values(5,'test','test','BrownsBay','Auckland')";
+            using (SqlConnection connection = new SqlConnection(
+                           connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                int result=command.ExecuteNonQuery();
+                Console.WriteLine("ExecuteResult: {0}",
+                            result);
+
+            }
+        }
+
+        [Test]
+        public void TestQuery()
+        {
+            string connectionString =@"Server = IVYGUO1002\SQLEXPRESS; Database = Test; Trusted_Connection = SSPI";
+            string queryString = "Select * from Persons";
+            using (SqlConnection connection = new SqlConnection(
+                           connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                IDataRecord record = (IDataRecord)reader;
+                while (reader.Read())
+                {
+                    Console.WriteLine(string.Format("{0},{1}\n"
+                    ,record[0], record[1]));
+                }
+                // Call Close when done reading.
+                reader.Close();
+            }
+        }
+        [Test]
+        public void TestDataSet()
+        {
+
+        }
+
+    }
+}
